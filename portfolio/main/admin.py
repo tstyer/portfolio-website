5723f62f64c6066ee96c5674ef440da53d21a996
tstyer <travisstyer.ts@gmail.com>
2025-10-21 10:31:51 +0100

    Add projectImageInline to admin.py

--- portfolio/main/admin.py
+++ portfolio/main/admin.py
@@ -3,8 +3,12 @@ from .models import Tag, Project, ProjectImage
 
 # Register your models here.
 
+class ProjectImageInline(admin.TabularInline):
+    model = ProjectImage
+    extra = 1 # Upload 1 image. 
+
 class ProjectAdmin(admin.ModelAdmin):
     list_display = ("title", "link") # When viewing the list of projects, we see the title and link. 
     inlines = [ProjectImageInline]
     search_fields = ("title", "description") # Can search based on title and/or description. 
-    list_filter = ("tags",)
+    list_filter = ("tags",) # Need trailing comma so it's treated as a tupple. 

040c4aa832113b60abd4c5cadd459e273713266a
tstyer <travisstyer.ts@gmail.com>
2025-10-21 12:38:10 +0100

    Add basic views foundations

--- portfolio/main/views.py
+++ portfolio/main/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Project, Tag
 
 # Create your views here.
+
+def Home(request):
+    return render(request, "home.html") # Rendering justr means to show on the screen.
+
+def contact(request):
+    return render(request, "contact.html")
+
+def project(request, id):
+    return render(request, "project.html")
+
+# Self-learn note:
+ # Views are functions called when wanting to display a page. 
+ # You need to import models to do so. 
+
+

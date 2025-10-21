5429f022a2e2cf86fd0bad377f8e5656b2869743
tstyer <travisstyer.ts@gmail.com>
2025-10-21 13:08:30 +0100

    Write the url paths in urls.py

--- portfolio/main/urls.py
+++ portfolio/main/urls.py
@@ -1,2 +1,9 @@
+from django.urls import path
+from . import views
+
 urlpatterns = [
+    path("", views.home, name="home"), # Going to nothing = home page.
+    path("home/", views.home, name="home"), # Standard home url. 
+    path("contact/", views.contact, name="contact"),
+    path("project/<int:id>/", views.project, name="project"),
 ]

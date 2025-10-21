64b115d9ab3c99de45599b0e32eebb8366377cc3
tstyer <travisstyer.ts@gmail.com>
2025-10-21 10:03:11 +0100

    Write image model.

--- portfolio/main/models.py
+++ portfolio/main/models.py
@@ -2,21 +2,29 @@ from django.db import models
 
 # Create your models here.
 
-class Project(models.Model):
-    title = models.CharField(max_length=200)
-    description = models.TextField
-    tags = models.ManyToManyField(Tag, related_name="projects")
-    link = models.URLField(max_length=200, blank=True) # Blank = True (Ok if no link for project).
-
-    def __str__(self):
-        return self.title
-    
-
 class Tag(models.Model):
     name = models.CharField(max_length=100, unique=True) # So there cannot be duplicate names.
 
     def __str__(self):
         return self.name
+
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField
+    tags = models.ManyToManyField(Tag, related_name="projects") # Many-to-many because many projects can have many tags. 
+    link = models.URLField(max_length=200, blank=True) # Blank = True (Ok if no link for project).
+
+    def __str__(self):
+        return self.title
+
+# Because we have multiple images per project, we need to create a separate model for the images.
+class ProjectImage(models.Model):
+    models.ForeignKey(Project, related_name="images", on_delete=models.CASCADE)
+    image = models.ImageField(upload_to="project_images/")
+
+    def __str__(self):
+        return f"{self.project.title} Image"
+    
 
 
 # *Self-learn note:

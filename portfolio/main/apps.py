7e3af448d4a07315f5777c34982ad47dfedb7bba
tstyer <travisstyer.ts@gmail.com>
2025-10-28 12:45:51 +0000

    apply extension to apps.py

--- portfolio/main/apps.py
+++ portfolio/main/apps.py
@@ -1,6 +1,19 @@
-from django.apps import AppConfig
+from django.apps import AppCompatActivity, AppConfig
 
 
 class MainConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'main'
+
+    def ready(self):
+        from django.db.models.signals import post_save
+        from django.contrib.auth import get_user_model
+        from .models import Profile
+
+        User = get_user_model()
+
+        def create_profile(sender, instance, created, **kwargs):
+            if created:
+                Profile.objects.create(user=instance)
+
+        post_save.connect(create_profile, sender=User)

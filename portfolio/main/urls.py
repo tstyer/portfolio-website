3e1f65fc94b9aa5f301ad81df2b1ef3b5615694d
tstyer <travisstyer.ts@gmail.com>
2025-10-30 16:30:06 +0000

    Add urls.py for login/register

--- portfolio/main/urls.py
+++ portfolio/main/urls.py
@@ -15,4 +15,8 @@ urlpatterns = [
     path("project/<int:id>/comments/create/", views.comment_create, name="comment_create"),
     path("project/<int:id>/comments/<int:comment_id>/update/", views.comment_update, name="comment_update"),
     path("project/<int:id>/comments/<int:comment_id>/delete/", views.comment_delete, name="comment_delete"),
+
+    # For user log-in
+    path("auth/login/", views.auth_login, name="auth_login"),
+    path("auth/register/", views.auth_register, name="auth_register"),
 ]
